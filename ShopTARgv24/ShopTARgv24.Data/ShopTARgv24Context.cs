3df3f4c009b3c119c15efb8a851f1699b378def2
d3ede <theskonel@gmail.com>
2025-09-02 19:02:33 +0300

    Database connect

---
+++ ShopTARgv24/ShopTARgv24.Data/ShopTARgv24Context.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ShopTARgv24.Core.Domain;
+
+namespace ShopTARgv24.Data
+{
+    public class ShopTARgv24Context : DbContext
+    {
+        public ShopTARgv24Context(DbContextOptions<ShopTARgv24Context> options)
+        : base(options) { }
+        public DbSet<Spaceship> Spaceships { get; set; }
+    }
+}
